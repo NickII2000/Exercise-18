bf356da982ecae1655dbe73c765e679074efed86
Nick <Nick_II2000@mail.ru>
2022-04-24 20:49:12 +0600

    10-th commit

--- script.js
+++ script.js
@@ -195,3 +195,53 @@ console.log(getCoupeNumber(0));
 console.log(getCoupeNumber(7.7));
 console.log(getCoupeNumber(-10));
 console.log(getCoupeNumber('Hello'));
+
+// // Упражнение 29 (задания)
+// б)
+
+function getTimeFromMinutes(minutesTotal) {
+    if (typeof (minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    const hours = Math.floor(minutesTotal / 60);
+    const minutes = minutesTotal % 60;
+
+    let hoursStr = '';
+
+    switch (hours) {
+        case 0:
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    }
+
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+console.log(getTimeFromMinutes(180));
+
+function findMaxNumber(a, b, c, d) {
+    // Самое простое - это использовать Math.max :)
+    // А оптимизировать такую проверку мы научимся совсем скоро
+    if (typeof (a) !== 'number' ||
+        typeof (b) !== 'number' ||
+        typeof (c) !== 'number' ||
+        typeof (d) !== 'number') {
+        return 0;
+    } else {
+        return Math.max(a, b, c, d);
+    }
+}
+
+console.log(findMaxNumber(1, 5, 6.6, 10.5));
+console.log(findMaxNumber(1, 5, '6', '10'));
