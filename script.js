f1f412f94b1ac6e4e7b44d0959d599f7cb8c0669
Nick <Nick_II2000@mail.ru>
2022-05-03 20:52:10 +0600

    28-th commit

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ let x = 5;
 console.log(x++);
 
 //Чему равно такое выражение: NaN
-[] + false - null + true;
+// [] + false - null + true;
 console.log([] + false - null + true);
 
 //Что выведет этот код: 2
@@ -322,30 +322,41 @@ let x = y = 2;
 console.log(x);
 
 //Чему равна сумма: 12
-[] + 1 + 2;
+// [] + 1 + 2;
 console.log([] + 1 + 2);
 
 //Что выведет этот код: "1"
-alert("1" [0])
+// alert("1" [0]);
+console.log("1" [0]);
+console.log(typeof "1" [0]);
 
 //Чему равно: null
-2 && 1 && null && 0 && undefined
+// 2 && 1 && null && 0 && undefined;
+console.log(2 && 1 && null && 0 && undefined);
 
-    //Есть ли разница между выражениями? нет
-    !!(a && b) и(a && b)
+//Есть ли разница между выражениями? нет
+// !!(a && b) и (a && b);
+let a = true,
+    b = false;
+console.log(!!(a && b) === (a && b));
 
 //Что выведет этот код: 3
-alert(null || 2 && 3 || 4);
+// alert(null || 2 && 3 || 4);
+console.log(null || 2 && 3 || 4);
 
 //Правда ли что a == b ? нет
-a = [1, 2, 3];
-b = [1, 2, 3];
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+console.log(a === b);
 
 //Что выведет этот код: infinity
-alert(+"Infinity");
+// alert(+"Infinity");
+console.log(+"Infinity");
 
 //Верно ли сравнение: нет
-"Ёжик" > "яблоко"
+// "Ёжик" > "яблоко";
+console.log("Ёжик" > "яблоко");
 
 //Чему равно: 2
-0 || "" || 2 || undefined || true || falsе
+// 0 || "" || 2 || undefined || true || falsе;
+console.log(0 || "" || 2 || undefined || true || falsе);
