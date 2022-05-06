02afcf5758bfa6501b48df948d6fac7556589b3f
Nick <Nick_II2000@mail.ru>
2022-05-06 11:44:17 +0600

    43-th commit

--- script.js
+++ script.js
@@ -467,3 +467,11 @@ console.log(arr); // 1,2,3,4,5
 
 arr.splice(-2, 2, 3, 4, 5);
 console.log(arr); // 1,2,3,4,5
+
+//[ 1, 2, 3, 4, 5 ]
+console.log(arr.slice(1, 3)); // 2, 3 (копирует с 1 до 3)
+console.log(arr.slice(-2)); // 4, 5 (копирует с -2 до конца)
+
+console.log(arr.slice(0)); // (копирует с 0 до конца)
+console.log(arr.slice(0, arr.length)); // (копирует с 0 до конца)
+console.log(arr.slice()); // (копирует с 0 до конца)
